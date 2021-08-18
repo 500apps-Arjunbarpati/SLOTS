df0038e8f41b52562424a772043c3f3b5ac8f0ec
500apps-Arjunbarpati <88705497+500apps-Arjunbarpati@users.noreply.github.com>
2021-08-18 19:39:45 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <form-helper>
+            <div slot="form-header">
+                <h3>Mantra Technologies</h3>
+                <p>please enter your name and password </p>
+            </div>
+            <div slot="form-fields">
+                <input type="text" placeholder="name" required />
+                <input type="password" placeholder="password" required />
+            </div>
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Submit</button>
+            </div>
+        </form-helper>
+    </div>
+</template>
+
+<script>
+// Imports
+import formHelper from './components/formHelper.vue'
+
+export default {
+    components: {
+        'form-helper': formHelper
+    },
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+        handleSubmit: function(){
+            alert('thanks for submitting');
+        }
+    }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+</style>
